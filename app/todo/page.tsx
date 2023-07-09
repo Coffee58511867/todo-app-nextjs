9c8efbcb5984053778b0df3c05ab06c8349280ff
Coffee58511867 <marobahlalele910@gmail.com>
2023-07-09 11:22:05 +0200

    Initial commit

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -1,35 +1,74 @@
 "use client";
 
-import { FormControl, FormLabel, Input, SimpleGrid , Button} from "@chakra-ui/react";
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  SimpleGrid,
+  Button,
+  Flex,
+  Stack,
+  useColorModeValue,
+  Box,
+} from "@chakra-ui/react";
 import { useForm } from "react-hook-form";
 import ITodo from "../models/todo.type";
 import { uploadError } from "../Validators/FormValidator";
 
-
-
 export default function AddItem() {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors, isSubmitting },
-      } = useForm<ITodo>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<ITodo>();
 
-    const onSubmit = async (data: ITodo) => {
+  const onSubmit = async (data: ITodo) => {
+    try {
         console.log(data);
+    } catch (error) {
+        console.log(error);
     }
+  };
   return (
-    <SimpleGrid p={20}>
+    <Flex
+    minH={"100vh"}
+    align={"center"}
+    justify={"center"}
+    bg={useColorModeValue("gray.50", "gray.800")}
+  >
+    <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
       <form onSubmit={handleSubmit(onSubmit)} noValidate>
+      <Box
+            rounded={"lg"}
+            bg={useColorModeValue("white", "gray.700")}
+            boxShadow={"lg"}
+            p={8}
+          >
+      <Stack spacing={4} py={13}>
         <FormControl isRequired>
           <FormLabel>Title</FormLabel>
-          <Input placeholder="Title"    {...register("title", uploadError.title)}/>
+          <Input
+            placeholder="Title"
+            {...register("title", uploadError.title)}
+          />
+          {errors?.title && <p className="error">{errors.title.message}</p>}
         </FormControl>
         <FormControl isRequired>
           <FormLabel>Description</FormLabel>
-          <Input placeholder="Description"   {...register("body", uploadError.body)} />
+          <Input
+            placeholder="Description"
+            {...register("body", uploadError.body)}
+          />
+          {errors?.body && <p className="error">{errors.body.message}</p>}
         </FormControl>
-        <Button size='lg' colorScheme='facebook' type="submit">Submit</Button>
+        <Button size="lg" colorScheme="facebook" type="submit" 
+        loadingText="Submitting" isLoading={isSubmitting}>
+          Submit
+        </Button>
+        </Stack>
+        </Box>
       </form>
-    </SimpleGrid>
+    </Stack>
+    </Flex>
   );
 }
