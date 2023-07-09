c830ef678525c6a44beb58f284f26ee5e1429155
Coffee58511867 <marobahlalele910@gmail.com>
2023-07-09 11:14:45 +0200

    Initial commit

---
+++ app/Validators/FormValidator.ts
@@ -0,0 +1,59 @@
+export const uploadError = {
+    firstName: {
+      required: "First name is required",
+      minLength: 3,
+      pattern: {
+        value: /^[a-zA-Z\u00C0-\u017F]+(?:\s[a-zA-Z\u00C0-\u017F]+)*$/,
+        message: "Invalid character!!",
+      },
+    },
+    lastName: {
+      required: "Last name is required",
+      minLength: 3,
+      pattern: {
+        value: /^[a-zA-Z\u00C0-\u017F]+(?:\s[a-zA-Z\u00C0-\u017F]+)*$/,
+        message: "Invalid character!!",
+      },
+    },
+    phoneNumber: {
+        required: "Phone number is required",
+        maxLength: 8,
+        minLength: 8,
+      },
+      emailAddress: {
+        required: "Email address is required",
+        pattern: {
+          value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+          message: "Invalid email address",
+        },
+      },
+      password: {
+        required: "Password is required",
+        pattern : {
+            value:  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$@!%&*?])[A-Za-z\d#$@!%&*?]{8,30}$/,
+            message: "Minimum eight characters, at least one letter, one number and one special character"
+        }
+      },
+      loginpassword: {
+        required: "Password is required",
+      },
+      username : {
+        required: "Username is required",
+      },
+      title: {
+        required: "Title is required",
+      },
+      body : {
+        required: "Body is required",
+      },
+  };
+
+
+//  { message for Password regex = Min 1 uppercase letter.
+//   Min 1 lowercase letter.
+//   Min 1 special character.
+//   Min 1 number.
+//   Min 8 characters.
+//   Max 30 characters.
+
+// }
--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -1,51 +1,35 @@
-export default function PageWithJSbasedForm() {
-    // Handles the submit event on form submit.
-    const handleSubmit = async (event: any) => {
-      // Stop the form from submitting and refreshing the page.
-      event.preventDefault()
-   
-      // Get data from the form.
-      const data = {
-        first: event.target.first.value,
-        last: event.target.last.value,
-      }
-   
-      // Send the data to the server in JSON format.
-      const JSONdata = JSON.stringify(data)
-   
-      // API endpoint where we send form data.
-      const endpoint = '/api/form'
-   
-      // Form the request for sending data to the server.
-      const options = {
-        // The method is POST because we are sending data.
-        method: 'POST',
-        // Tell the server we're sending JSON.
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        // Body of the request is the JSON data we created above.
-        body: JSONdata,
-      }
-   
-      // Send the form data to our forms API on Vercel and get a response.
-      const response = await fetch(endpoint, options)
-   
-      // Get the response data from server as JSON.
-      // If server returns the name submitted, that means the form works.
-      const result = await response.json()
-      alert(`Is this your full name: ${result.data}`)
+"use client";
+
+import { FormControl, FormLabel, Input, SimpleGrid , Button} from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
+import ITodo from "../models/todo.type";
+import { uploadError } from "../Validators/FormValidator";
+
+
+
+export default function AddItem() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+      } = useForm<ITodo>();
+
+    const onSubmit = async (data: ITodo) => {
+        console.log(data);
     }
-    return (
-      // We pass the event to the handleSubmit() function on submit.
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="first">First Name</label>
-        <input type="text" id="first" name="first" required />
-   
-        <label htmlFor="last">Last Name</label>
-        <input type="text" id="last" name="last" required />
-   
-        <button type="submit">Submit</button>
+  return (
+    <SimpleGrid p={20}>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
+        <FormControl isRequired>
+          <FormLabel>Title</FormLabel>
+          <Input placeholder="Title"    {...register("title", uploadError.title)}/>
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel>Description</FormLabel>
+          <Input placeholder="Description"   {...register("body", uploadError.body)} />
+        </FormControl>
+        <Button size='lg' colorScheme='facebook' type="submit">Submit</Button>
       </form>
-    )
-  }
+    </SimpleGrid>
+  );
+}
