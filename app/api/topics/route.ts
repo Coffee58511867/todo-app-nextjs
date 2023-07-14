94195dc0d48498fe0175a159140e6843cf99f1df
Coffee58511867 <marobahlalele910@gmail.com>
2023-07-14 08:58:43 +0200

    Added delete function

--- app/api/topics/route.ts
+++ app/api/topics/route.ts
@@ -6,23 +6,41 @@ export async function POST(request: Request) {
   const { title, description } = await request.json();
   await connectMongoDB();
   await topic.create({ title, description });
-  return NextResponse.json(
-    { message: "Topic Created" },
-    {
-      status: 201,
-    }
-  );
+  try {
+    return NextResponse.json(
+      { message: "Topic Created" },
+      {
+        status: 201,
+      }
+    );
+  } catch (error) {
+    return NextResponse.json(
+        { message: "Topic not Created", error },
+        {
+          status: 400,
+        }
+      );
+  }
 }
 
 export async function GET() {
   await connectMongoDB();
   const topics = await topic.find();
-  return NextResponse.json({ topics });
+  try {
+    return NextResponse.json({ topics });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error", error },
+      {
+        status: 500,
+      }
+    );
+  }
 }
 
 export async function DELETE(request: NextRequest) {
-    const id = request.nextUrl.searchParams.get("id");
-    await connectMongoDB();
-    await topic.findByIdAndDelete(id);
-        return NextResponse.json({ message : "Topic Deleted" }, {status: 200});
-  }
+  const id = request.nextUrl.searchParams.get("id");
+  await connectMongoDB();
+  await topic.findByIdAndDelete(id);
+  return NextResponse.json({ message: "Topic Deleted" }, { status: 200 });
+}
