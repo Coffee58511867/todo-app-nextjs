d31eff6a56f2b2221ce697d113e3099cfb4861f3
Coffee58511867 <marobahlalele910@gmail.com>
2023-07-15 16:42:09 +0200

    Created Login api

--- app/api/auth/v1/login/route.ts
+++ app/api/auth/v1/login/route.ts
@@ -5,7 +5,9 @@ import { NextRequest, NextResponse } from "next/server";
 import { compare } from 'bcryptjs'
 import { serialize } from "cookie";
 
-const MAX_AGE = 60 * 60 * 24 * 30; // 30 days
+//const MAX_AGE = 60 * 60 * 24 * 30; // 30 days
+
+const MAX_AGE = 60 * 60 * 12;; //12 HRS
 
 export async function POST(request: Request) {
   const body = await request.json();
