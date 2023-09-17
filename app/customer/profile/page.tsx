3854abdf4e3db0ebb4a0a5a2c67bb3e94d05032f
Coffee58511867 <marobahlalele910@gmail.com>
2023-09-17 06:23:12 +0200

    Created customer pages

--- app/customer/profile/page.tsx
+++ app/customer/profile/page.tsx
@@ -1,17 +1,21 @@
 "use client"
 
+import IUserRegiter from "@/app/models/admin.type";
 import { Text } from "@chakra-ui/react"
 import axios, { isAxiosError } from "axios";
 import { error } from "console";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Profile(){
+
+    const [userProfile , setUserProfile] = useState<IUserRegiter[]>([]);
   
     const userId = localStorage.getItem("userId");
      console.log(userId);
      useEffect(() => {
        axios.get('/api/auth/register/' +userId).then((response) => {
         console.log(response.data.user);
+        setUserProfile(response.data.user);
        }).catch((error) => {
         console.log(error);
        })
