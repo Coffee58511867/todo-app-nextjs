df88f3bb1168bab7f9827f73bc63ed262414f5c6
Coffee58511867 <marobahlalele910@gmail.com>
2023-09-17 12:24:40 +0200

    Created customer pages

---
+++ app/customer/bookings/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+import { Text } from "@chakra-ui/react";
+
+export default function BookingList(){
+    
+
+    return(
+        <>
+        <Text>
+            Booking List
+        </Text>
+        
+        </>
+    )
+}
