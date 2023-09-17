2c5c1f1a5ac56898d6990c6266d7447ff6a796cf
Coffee58511867 <marobahlalele910@gmail.com>
2023-09-17 06:21:26 +0200

    Created customer pages

--- app/customer/profile/page.tsx
+++ app/customer/profile/page.tsx
@@ -1,11 +1,25 @@
 "use client"
 
 import { Text } from "@chakra-ui/react"
+import axios, { isAxiosError } from "axios";
+import { error } from "console";
+import { useEffect } from "react";
 
 export default function Profile(){
   
     const userId = localStorage.getItem("userId");
      console.log(userId);
+     useEffect(() => {
+       axios.get('/api/auth/register/' +userId).then((response) => {
+        console.log(response.data.user);
+       }).catch((error) => {
+        console.log(error);
+       })
+    
+
+     }, [userId]);
+
+     
     return(
         <div>
             <Text fontSize={'4xl'}>Welcome to M4K Laundry {userId}</Text>
